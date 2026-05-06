1bb7dba307c39d61a6c584d41770649bb8734c1c
Camorany <Cjlane9090@gmail.com>
2026-05-06 23:43:17 +0100

    header sizing

--- camorany-portfolio/src/components/Home.vue
+++ camorany-portfolio/src/components/Home.vue
@@ -2,8 +2,8 @@
 </script>
 
 <template> 
-    <div class="flex flex-col items-center justify-center min-h-screen m-8 outline-2 outline-blue-500 p-3 gap-10">
-        <h1>Cameron Lane</h1>
+    <div class="flex flex-col items-center justify-center min-h-screen m-8 outline-2 outline-blue-500 p-3 gap-10 text-white">
+        <h1 class="text-4xl font-bold">Cameron Lane</h1>
 
 
         <div class="flex gap-20">
