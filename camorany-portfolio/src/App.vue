1c6b77f4b6790ca49c1e5f91491937aefb2e6849
Camorany <Cjlane9090@gmail.com>
2026-05-19 23:12:08 +0100

    contact section boilerplate

--- camorany-portfolio/src/App.vue
+++ camorany-portfolio/src/App.vue
@@ -4,6 +4,7 @@ import Home from './components/Home.vue';
 import AboutMe from './components/AboutMe.vue';
 import Projects from './components/Projects.vue';
 import Experience from './components/Experience.vue';
+import Contact from './components/Contact.vue';
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import Experience from './components/Experience.vue';
     <AboutMe />
     <Projects />
     <Experience />
+    <Contact />
   </div>
 </template>
 
---
+++ camorany-portfolio/src/components/Contact.vue
@@ -0,0 +1,36 @@
+<script setup> 
+import { Code } from 'lucide-vue-next'
+
+</script>
+
+<template>
+  <section
+    id="contact" class="min-h-screen bg-gray-900 text-slate-300 flex items-center justify-center px-30 py-35 lg:py-30"
+  >
+
+  <!-- MAIN CONTAINER -->
+    <div class="max-w-7xl w-full">
+
+      <!-- TITLE -->
+      <div class="text-center mb-14">
+            <h2 class="text-5xl font-bold text-emerald-600 mb-3">
+            Contact Me
+            </h2>
+      </div>
+
+      <div class="flex flex-col lg:flex-row gap-8 p-2">
+      
+            <!-- LEFT SIDE -->
+            <div class="lg:w-[55%] w-full order-2 lg:order-1">
+            </div>
+
+            <!-- RIGHT SIDE -->
+            <div class="flex justify-center items-center lg:w-[45%] w-full md:text-left order-1 lg:order-2">
+            </div>
+
+      </div>
+
+    </div>
+
+  </section>
+</template>
